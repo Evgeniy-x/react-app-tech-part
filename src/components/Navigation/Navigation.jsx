f9e77192d0a8933345e1446933f6c31d49fa23c1
Evgeniy <yaremenko.x@gmail.com>
2023-10-27 17:11:04 +0300

    add Navegation

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,20 @@
+import { NavLink } from 'react-router-dom';
+import './Navigation.scss';
+import { IoHome } from 'react-icons/io5';
+
+export default Navigation = () => (
+  <nav className="navigation">
+    <ul>
+      <li>
+        <NavLink to="/">
+          <IoHome />
+        </NavLink>
+      </li>
+
+      <li>
+        <NavLink to="/catalog">Catalog</NavLink>
+        <NavLink to="/favorites">Favorites</NavLink>
+      </li>
+    </ul>
+  </nav>
+);
