d045c2dc0091512c63ebaa4673385a67bee9a2b2
Evgeniy-x <117513013+Evgeniy-x@users.noreply.github.com>
2023-12-22 19:48:04 +0000

    Deploying to gh-pages from @ Evgeniy-x/react-app-tech-part@e3a75cb258d49674d1cb421e669b37a35ad2cccd 🚀

---
+++ static/js/263.cd8c53f4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_app_tech_part=self.webpackChunkreact_app_tech_part||[]).push([[263],{263:function(e,t,a){a.r(t),t.default={}}}]);
+//# sourceMappingURL=263.cd8c53f4.chunk.js.map
